8ccc8074dada3d6cece80f9a9950de80a5cedf6b
Dmytro Hensirovskyi <dmytrohensirovskyi@MacBookAir.fritz.box>
2025-05-31 20:35:37 +0200

    Final build and push of frontend/dist for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Crypto-App/",
+  base: "/react-crypto-app/",
   build: {
     outDir: 'dist',
   },
